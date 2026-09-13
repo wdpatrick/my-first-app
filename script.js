1f987c497bb0f61db18664d09ee938d805193007
wdpatrick <bill.patrick@btinternet.com>
2026-09-13 22:56:34 +0100

    Add reset button

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function increaseNumber() {
     number = number + 10;
     document.getElementById("number").textContent = number;
 }
+function resetNumber() {
+    number = 0;
+    document.getElementById("number").textContent = number;
+}
+
+
